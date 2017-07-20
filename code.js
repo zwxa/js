8802b0e47913de4a9c4e3145ccf0778efca5e39d
zwxa <593693444@qq.com>
2017-07-20 14:29:17 +0800

    exercise
    exercise

--- code.js
+++ code.js
@@ -200,3 +200,39 @@ function partialUsingArguments(fn) {
      }
      return result;
  }
+ /*已知 fn 为一个预定义函数，实现函数 curryIt，调用之后满足如下条件：
+1、返回一个函数 a，a 的 length 属性值为 1（即显式声明 a 接收一个参数）
+2、调用 a 之后，返回一个函数 b, b 的 length 属性值为 1
+3、调用 b 之后，返回一个函数 c, c 的 length 属性值为 1
+4、调用 c 之后，返回的结果与调用 fn 的返回值一致
+5、fn 的参数依次为函数 a, b, c 的调用参数*/
+function curryIt(fn) {
+    var length = fn.length,
+        args = [];
+    var result =  function (arg){
+        args.push(arg);
+        length --;
+        if(length <= 0 ){
+            return fn.apply(this, args);
+        } else {
+            return result;
+        }
+    }    
+    return result;
+}
+
+/*返回参数 a 和 b 的逻辑且运算结果。*/
+function and(a, b) {
+    return !!(a && b)
+    //如果a为true，b为非Boolean就会返回非Boolean值，所以加一步转换
+}
+/*给定二进制字符串，将其换算成对应的十进制数字*/
+function base10(str) {
+    /**
+        其它进制转十进制
+        parseInt(str,2)
+        parseInt(str,8)
+        parseInt(str,16)
+    */
+    return parseInt(str,2);
+}
